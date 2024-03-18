47bd33ae7a30199032420ad51b1535f3cf7288da
chenzhixin346 <148316339+chenzhixin346@users.noreply.github.com>
2024-03-18 17:18:30 +0800

    更新了换装的界面

---
+++ src/views/HomePage.vue
@@ -0,0 +1,133 @@
+<template>
+  <div>
+    <div class="bottomm"></div>
+    <div class="Swiper">
+      <div class="Swiper-content">
+        <div class="Swiper-item" v-for="(item, index) in imageUrl" :key="index">
+          <img :src="item" alt="" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { Swiper } from "@/swiper/Swiper";
+export default {
+  name: "Swiper",
+  data() {
+    return {
+      imageUrl: [
+        require("../assets/0.jpg"),
+        require("../assets/1.jpg"),
+        require("../assets/2.jpg"),
+        require("../assets/3.jpg"),
+        require("../assets/4.jpg"),
+        require("../assets/5.jpg"),
+
+      ],
+      classList: ["one", "two", "three", "four", "five"],
+    };
+  },
+  methods: {
+    swiper() {
+      new Swiper({
+        classList: this.classList,
+        SwiperContent: ".swiper-content",
+      });
+    },
+  },
+
+  mounted() {
+    this.swiper();
+  },
+};
+</script>
+
+
+<style scoped>
+.titelText {
+  position: absolute;
+  font-size: 50px;
+  width: 100%;
+  top: 20px;
+  text-align: center;
+  font-family: "YouYuan";
+  letter-spacing: 8px;
+  color:white;
+  text-shadow: 0 0 0px #fff, 0 0 5px #fff, 0 0 10px #fff, 0 0 15px #00ffea,
+  0 0 20px #00ffd5, 0 0 30px #00eeff, 0 0 40px #00ffff, 0 0 50px #00fff2;
+}
+.bottomm{
+  height:150px;
+  width:100%;
+  position:absolute;
+  bottom:0;
+
+  left:0;
+  right:0;
+  background-size:100%;
+  background-repeat:no-repeat;
+  border-top:30px solid rgba(20, 20, 20, 0.5);
+  box-shadow: 0 0 10px rgb(255, 243, 187);
+}
+.Swiper {
+  height: 250px;
+  width: 100%;
+  position: relative;
+  margin: 150px auto;
+}
+
+.Swiper-content {
+  height: 100%;
+  width: 50%;
+  position: relative;
+  margin: 0 auto;
+  left:15px
+
+}
+
+.Swiper-content div {
+  position: absolute;
+  height: 150px;
+  width: 120px;
+  margin-top: 50px;
+  transition: all 0.6s;
+}
+.Swiper-content img {
+  height: 100%;
+  width: 100%;
+}
+.one {
+  z-index: 1;
+  transform: scale(0.8);
+  left: 75px;
+  box-shadow: -3px 4px 10px 1px rgba(0, 0, 0, 0.2);
+}
+.two {
+  z-index: 2;
+  transform: scale(0.9);
+  left: 175px;
+  box-shadow: -3px 4px 10px 1px rgba(0, 0, 0, 0.2);
+}
+
+.three {
+  z-index: 9;
+  transform: scale(1);
+  left: 275px;
+  box-shadow: -3px 4px 10px 1px rgba(0, 0, 0, 0.2);
+}
+.four {
+  z-index: 2;
+  transform: scale(0.9);
+  left: 375px;
+  box-shadow: 3px 4px 10px 1px rgba(0, 0, 0, 0.2);
+}
+
+.five {
+  z-index: 1;
+  transform: scale(0.8);
+  left: 475px;
+  box-shadow: 3px 4px 10px 1px rgba(0, 0, 0, 0.2);
+}
+</style>
--- src/views/PuppetTransfer.vue
+++ src/views/PuppetTransfer.vue
@@ -1,34 +1,81 @@
 <template>
   <div class="puppet" id="puppet-v">
     <div class="lead">
-      <div class="puppetImg-div" @click="toGetImg">
-        <img class="puppetImg" :src=valueUrl v-if="valueUrl">
+      <div class="puppetImg-div" >
+        <video class="puppetImg" :src="valueUrl" v-if="valueUrl" controls>
+        </video>
         <img
             class="puppetimage-circle"
             src="@/assets/puppet/frame.png"
-            alt="闽南剪纸迁移"
+            alt="木偶头表情迁移"
         >
       </div>
-      <img src="@/assets/puppet/leadbutton.png" class="puppetTag" @click="toGetImg">
+      <img src="@/assets/puppet/leadbutton.png" class="puppetTag" @click="toGetVideo">
     </div>
     <div class="build">
       <div class="puppetImg-div" >
-        <img class="puppetImg" :src=outUrl v-if="outUrl">
+        <video class="puppetImg" v-if="GeneratedVideo" controls>
+          <source src="@/assets/puppet/video/video1.mp4" type="video/mp4">
+        </video>
         <img
             class="puppetimage-circle"
             src="@/assets/puppet/frame.png"
-            alt="闽南剪纸迁移"
+            alt="木偶头表情迁移"
         >
       </div>
-      <img src="@/assets/puppet/buildbutton.png" class="puppetTag" @click="downloadImage">
+      <img src="@/assets/puppet/buildbutton.png" class="puppetTag" @click="GetGeneratedVideo">
     </div>
   </div>
 
 </template>
 
 <script>
+import axios from 'axios';
+
+let inputElement = null;
 export default {
-  name: "PuppetTransfer"
+  name: "PuppetTransfer",
+  data() {
+    return {
+      valueUrl: '',
+      GeneratedVideo : false
+    };
+  },
+  methods: {
+    toGetVideo() {
+      if (inputElement === null) {
+        inputElement = document.createElement('input');
+        inputElement.setAttribute('type', 'file');
+        inputElement.setAttribute('accept', 'video/*');
+        inputElement.style.display = 'none';
+
+        if (window.addEventListener) {
+          inputElement.addEventListener('change', this.uploadVideo, false);
+        } else {
+          inputElement.attachEvent('onchange', this.uploadVideo);
+        }
+
+        document.body.appendChild(inputElement);
+      }
+      inputElement.click();
+    },
+    uploadVideo(event) {
+      if (event && event.target && event.target.files && event.target.files.length > 0) {
+        const file = event.target.files[0];
+        const reader = new FileReader();
+        reader.onload = () => {
+          // 将视频显示在页面中
+          this.valueUrl = reader.result;
+        };
+        reader.readAsDataURL(file);
+      }
+    },
+    GetGeneratedVideo(){
+      setTimeout(() => {
+        this.GeneratedVideo = true;
+      }, 3000); // 延迟3秒（3000毫秒）后执行
+    }
+  },
 }
 </script>
 
@@ -41,7 +88,7 @@ export default {
 }
 
 .puppetImg-div {
-  width:  295px;
+  width:  314px;
   height:  314px;
   overflow: hidden;
   cursor: pointer;
@@ -51,12 +98,13 @@ export default {
 
 .puppetImg {
   display: block;
-  width: 273px;
-  height: 273px;
-  margin-left: 52px;
-  margin-top: 49px;
+  width: 178px;
+  height: 188px;
+  margin-left: 81px;
+  margin-top: 96px;
   z-index: 999;
   position: absolute;
+  border-radius: 5%;
 }
 #puppet-v {
   background: url("../assets/puppet/bg.jpg");
@@ -65,13 +113,15 @@ export default {
   background-size: 100% 100%;
 }
 .puppetimage-circle{
-  width: 304px;
-  height: auto;
+  width: 360px;
+  height: 360px;
 }
 .puppetTag{
   width: 200px;
   height: 62px;
   cursor: pointer;
+  margin-left: 27px;
+  margin-top: 10px;
 }
 .puppetTag:hover {
   transform: scale(1.1); /* 放大效果 */
@@ -81,14 +131,14 @@ export default {
   flex-direction: column;
   justify-content: flex-start;
   align-items: center;
-  margin-top: 80px;
+  margin-top: 50px;
 }
 .build{
   display:flex;
   flex-direction: column;
   justify-content: flex-start;
   align-items: center;
-  margin-top: 80px;
+  margin-top: 50px;
 }
 
 </style>
